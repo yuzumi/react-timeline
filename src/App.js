4996dc0bb14826b736f67eed612d9beca219011f
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 15:20:47 +0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Timeline from 'components/Timeline';
+
+import data from 'data';
 
 const App = () => (
   <main className="app">
-
+    <Timeline {...{data}} />
   </main>
 );
 
