d93905fffc6c21b5b2e6424a7224c92923d7c5fb
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 15:20:19 +0300

    Add dummy data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    text: 'Wrote my first blog post',
+    datetime: 'July 07 2019',
+    tag: 'react',
+  }, {
+    id: 2,
+    text: 'Wrote my second blog post',
+    datetime: 'July 08 2019',
+    tag: 'javascript',
+  }, {
+    id: 3,
+    text: 'Wrote my third blog post',
+    datetime: 'July 09 2019',
+    tag: 'node',
+  }, {
+    id: 4,
+    text: 'Wrote my fourth blog post',
+    datetime: 'July 10 2019',
+    tag: 'javascript',
+  },
+];
