3fa2cac18412f60baa1ad9dfc4ed4b7b0d4f10c5
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-07 15:22:33 +0300

    Import css and js files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import App from './App';
+import App from 'App';
 
-import './index.css';
+import 'styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
