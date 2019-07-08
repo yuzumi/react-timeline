883d3760c1da94edc6456ea1f538f177a0f4d069
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-08 19:58:19 +0300

    Modify modifier destination logic

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import TimelineItem from 'components/TimelineItem';
+import { isOdd } from 'helpers';
 
 const Timeline = ({ data }) => {
-  const isEven = value => value % 2 === 0;
-
   const renderItem = (data, index) => (
     <TimelineItem  
       {...{
         data, 
         key: data.id,
-        isEven: isEven(index)
+        isOdd: isOdd(index) 
       }} 
     />
   );
--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const TimelineItem = ({ data, isEven }) => (
+const TimelineItem = ({ data, isOdd }) => (
   <div 
     className={classNames(
       'timeline-item', {
-      'timeline-item--is-even': isEven,
-      'timeline-item--is-odd': !isEven,
+      'timeline-item--is-odd': isOdd
     })}
   >
     <main className="timeline-item__content">
